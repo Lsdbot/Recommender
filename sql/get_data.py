a7e8fff874fa004bb6328056d384972727e81feb
Sergey <s_levsha@bk.ru>
2023-03-08 11:47:53 +0300

    Rename

--- sql/get_data.py
+++ sql/get_data.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def csv_to_sql(filepath, table_name):
+def load_data(filepath, table_name):
     db = pd.read_csv(filepath)
     db = db.T
     conn = create_engine("sqlite:///recommender.db")
     db.to_sql(name=table_name, con=conn, if_exists="replace")
 
-def read_sql_table(sql_path, table_name):
+def read_data(sql_path, table_name):
     conn = create_engine(sql_path)
     return pd.read_sql_table(table_name, conn.connect())
