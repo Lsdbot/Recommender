b8e1162991b8d32b11955bcf40d81f9e882d64d8
Sergey <s_levsha@bk.ru>
2023-03-09 22:41:48 +0300

    Experiments

--- sqlite/get_data.py
+++ sqlite/get_data.py
@@ -2,10 +2,19 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def load_data(filepath, table_name, if_exists="replace", chunksize=100000):
-    db = pd.read_parquet(filepath, engine='pyarrow')
+def get_data(filepath) -> pd.DataFrame:
+    return pd.read_parquet(filepath, engine='pyarrow')
+
+
+def transform_data(df: pd.DataFrame, n_columns) -> pd.DataFrame:
+    
+    return
+
+
+def load_data(df: pd.DataFrame, table_name, if_exists="replace", chunksize=100000) -> None:
     conn = create_engine("sqlite:///sqlite/clients.db")
-    db.to_sql(name=table_name, con=conn, if_exists=if_exists, chunksize=chunksize)
+    df.to_sql(name=table_name, con=conn, if_exists=if_exists, chunksize=chunksize)
+
 
 def read_data(sql_path, table_name):
     conn = create_engine(sql_path)
